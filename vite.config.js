c41770a45f016b8966e440247e2b436876d2d6c1
valcsevdaniel <valcsev.dani@gmail.com>
2024-03-31 16:07:07 +0200

    new config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/<REPO>/', // Replace '<REPO>' with your repository name
+  base: './', 
   plugins: [react()]
 });

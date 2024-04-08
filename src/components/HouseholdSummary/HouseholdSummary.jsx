be74bf7cfd61107ba7660be0cb5f0416e7174829
valcsevdaniel <valcsev.dani@gmail.com>
2024-04-08 10:17:28 +0200

    Sum

--- src/components/HouseholdSummary/HouseholdSummary.jsx
+++ src/components/HouseholdSummary/HouseholdSummary.jsx
@@ -1,6 +1,13 @@
 import TableRow from "./components/TableRow";
 
 const HouseholdSummary = ({familyMemberPay}) => {
+    const calculateSum = () => {
+        let sum = 0;
+        familyMemberPay.forEach(element => {
+            sum = element.netPay + sum;
+        });
+        return sum;
+      };
   return (
   <div className=" ml-2 mr-2 w-full h-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-200 dark:border-gray-700 max-w-lg mx-auto lg:max-w-3xl xl:max-w-4xl">
     <h1 className="lg:mb-4 text-l font-bold leading-none tracking-tight text-gray-900 md:text-xl lg:text-3xl dark:text-black text-center"><span className="text-blue-600 dark:text-blue-500">Család</span> nettó bére </h1>
@@ -23,6 +30,7 @@ const HouseholdSummary = ({familyMemberPay}) => {
             {familyMemberPay.map((member) => (
               <TableRow key={member.id} name={member.name} netpay={member.netPay} />
             ))}
+                <TableRow key={-1} name={"Összesen"} netpay={calculateSum()}/>
            
         </thead>
     </table>
